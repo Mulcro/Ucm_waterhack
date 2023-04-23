0a245d44841d32b8f77b256ffe68ff6565a5a035
Mulero Alamou <Mulero.alamou@gmail.com>
2023-04-22 20:29:12 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Comp from './components/comp';
+
+function App() {
+  return (
+    <div className="App">
+      <Comp/>
+    </div>
+  );
+}
+
+export default App;
